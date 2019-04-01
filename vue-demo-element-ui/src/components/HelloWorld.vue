3d26a9f17b50c22d6e668e2cfa470e558e3265ed
y.liu@herin.ai <y.liu@herin.ai>
2019-04-01 12:03:50 +0800

    test to use element-ui in vue

---
+++ vue-demo-element-ui/src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <el-button>test</el-button>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import { Button } from 'element-ui'
+
+Vue.use(Button)
+
+export default {
+  name: 'HelloWorld'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
