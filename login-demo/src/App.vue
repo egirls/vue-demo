88398fc2b5925a03bf7e0854d658e149bc3ec150
y.liu@herin.ai <y.liu@herin.ai>
2019-04-02 17:42:22 +0800

    test table-element-ui

--- login-demo/src/App.vue
+++ login-demo/src/App.vue
@@ -2,91 +2,28 @@
   <div id="app">
     <el-container>
       <el-header>
-        <el-menu :default-active="activeIndex2" class="el-menu-demo" mode="horizontal" @select="handleSelect" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
-          <el-menu-item index="1">
-            <router-link to="/" target="_blank">处理中心</router-link>
-          </el-menu-item>
-          <el-submenu index="2">
-            <template slot="title">我的工作台</template>
-            <el-submenu index="2-1">
-              <template slot="title">选项1</template>
-              <el-menu-item index="2-1-1">选项1</el-menu-item>
-              <el-menu-item index="2-1-2">选项2</el-menu-item>
-              <el-menu-item index="2-1-3">选项3</el-menu-item>
-            </el-submenu>
-            <el-menu-item index="2-2">选项2</el-menu-item>
-            <el-menu-item index="2-3">选项3</el-menu-item>
-            <el-submenu index="2-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="2-4-1">选项1</el-menu-item>
-              <el-menu-item index="2-4-2">选项2</el-menu-item>
-              <el-menu-item index="2-4-3">选项3</el-menu-item>
-            </el-submenu>
-          </el-submenu>
-          <el-menu-item index="3">消息中心</el-menu-item>
-          <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
-        </el-menu>
+        <Header />
       </el-header>
       <el-container>
         <el-aside>
-          <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
-            <el-submenu index="1">
-              <template slot="title">
-                <i class="el-icon-location"></i>
-                <span>导航一</span>
-              </template>
-              <el-menu-item-group>
-                <template slot="title">分组一</template>
-                <el-menu-item index="1-1">选项1</el-menu-item>
-                <el-menu-item index="1-2">选项2</el-menu-item>
-              </el-menu-item-group>
-              <el-menu-item-group title="分组2">
-                <el-menu-item index="1-3">选项3</el-menu-item>
-              </el-menu-item-group>
-              <el-submenu index="1-4">
-                <template slot="title">选项4</template>
-                <el-menu-item index="1-4-1">选项1</el-menu-item>
-              </el-submenu>
-            </el-submenu>
-            <el-menu-item index="2">
-              <i class="el-icon-menu"></i>
-              <span slot="title">导航二</span>
-            </el-menu-item>
-            <el-menu-item index="3">
-              <i class="el-icon-document"></i>
-              <span slot="title">导航三</span>
-            </el-menu-item>
-            <el-menu-item index="4">
-              <i class="el-icon-setting"></i>
-              <span slot="title">导航四</span>
-            </el-menu-item>
-          </el-menu>
+          <Aside />
         </el-aside>
         <el-main>
-          <router-view />
+          <Content />
         </el-main>
       </el-container>
     </el-container>
   </div>
 </template>
 <script>
+import Header from './components/common/Header.vue'
+import Aside from './components/common/Aside.vue'
+import Content from './components/common/Content.vue'
 export default {
-  data () {
-    return {
-      activeIndex: '1',
-      activeIndex2: '1'
-    }
-  },
-  methods: {
-    handleSelect (key, keyPath) {
-      console.log(key, keyPath)
-    },
-    handleOpen (key, keyPath) {
-      console.log(key, keyPath)
-    },
-    handleClose (key, keyPath) {
-      console.log(key, keyPath)
-    }
+  components: {
+    Header,
+    Aside,
+    Content
   }
 }
 
@@ -110,7 +47,7 @@ li a {
   background-color: #545c64;
 }
 
-.el-aside>ul {
+.el-aside > ul {
   width: 100%
 }
 
--- login-demo/src/router.js
+++ login-demo/src/router.js
@@ -1,28 +1,35 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './components/common/Home.vue'
-import Login from './components/pages/Login.vue'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
+  routes: [{
+    path: '/',
+    component: () => import('./components/common/Content.vue'),
+    children: [{
+      path: '',
+      name: 'Home',
+      component: () => import('./components/common/Home.vue')
     },
     {
-      path: '/login',
-      name: 'Login',
-      component: Login
-    },
-    {
-      path: '*',
-      name: '404',
-      component: () => import('./components/pages/404.vue')
+      path: 'personal-message-table',
+      name: 'PersonalMessageTßable',
+      component: () => import('./components/common/PersonalMessageTable.vue')
     }
+    ]
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('./components/pages/Login.vue')
+  },
+  {
+    path: '*',
+    name: '404',
+    component: () => import('./components/pages/404.vue')
+  }
   ]
 })
