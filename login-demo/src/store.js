dabaab5809f7fb5b7eab7ed559d461f2376e6980
y.liu@herin.ai <y.liu@herin.ai>
2019-04-02 17:59:49 +0800

    add getters

--- login-demo/src/store.js
+++ login-demo/src/store.js
@@ -5,7 +5,28 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    tableData: [{
+      date: '2016-05-02',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1518 弄'
+    }, {
+      date: '2016-05-04',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1517 弄'
+    }, {
+      date: '2016-05-01',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1519 弄'
+    }, {
+      date: '2016-05-03',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1516 弄'
+    }]
+  },
+  getters: {
+    allMessage: (state) => {
+      return state.tableData
+    }
   },
   mutations: {
 
