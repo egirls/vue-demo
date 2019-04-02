96196e60310872601f358a56a2d8cc83f30ca9c7
y.liu@herin.ai <y.liu@herin.ai>
2019-04-02 17:45:27 +0800

    change router-use-type with element-ui

--- login-demo/src/components/common/Header.vue
+++ login-demo/src/components/common/Header.vue
@@ -1,8 +1,6 @@
 <template>
-  <el-menu :default-active="activeIndex2" class="el-menu-demo" mode="horizontal" @select="handleSelect" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
-    <el-menu-item index="1">
-      <router-link to="/">处理中心</router-link>
-    </el-menu-item>
+  <el-menu :default-active="this.$route.path" router class="el-menu-demo" mode="horizontal" @select="handleSelect" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+    <el-menu-item index="/">处理中心</el-menu-item>
     <el-submenu index="2">
       <template slot="title">我的工作台</template>
       <el-submenu index="2-1">
@@ -11,9 +9,7 @@
         <el-menu-item index="2-1-2">选项2</el-menu-item>
         <el-menu-item index="2-1-3">选项3</el-menu-item>
       </el-submenu>
-      <el-menu-item index="2-2">
-        <router-link to="/personal-message-table">选项2</router-link>
-      </el-menu-item>
+      <el-menu-item index="personal-message-table">选项2</el-menu-item>
       <el-menu-item index="2-3">选项3</el-menu-item>
       <el-submenu index="2-4">
         <template slot="title">选项4</template>
