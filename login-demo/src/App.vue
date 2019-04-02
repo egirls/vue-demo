7540dbb27b5ce007bf0c178d5ec4023c11f82d49
y.liu@herin.ai <y.liu@herin.ai>
2019-04-02 10:28:54 +0800

    change layout

--- login-demo/src/App.vue
+++ login-demo/src/App.vue
@@ -1,75 +1,72 @@
 <template>
   <div id="app">
-    <el-menu
-    :default-active="activeIndex2"
-    class="el-menu-demo"
-    mode="horizontal"
-    @select="handleSelect"
-    background-color="#545c64"
-    text-color="#fff"
-    active-text-color="#ffd04b">
-      <el-menu-item index="1">
-        <router-link to="/" target="_blank">处理中心</router-link>
-      </el-menu-item>
-      <el-submenu index="2">
-        <template slot="title">我的工作台</template>
-        <el-submenu index="2-1">
-          <template slot="title">选项1</template>
-          <el-menu-item index="2-1-1">选项1</el-menu-item>
-          <el-menu-item index="2-1-2">选项2</el-menu-item>
-          <el-menu-item index="2-1-3">选项3</el-menu-item>
-        </el-submenu>
-        <el-menu-item index="2-2">选项2</el-menu-item>
-        <el-menu-item index="2-3">选项3</el-menu-item>
-        <el-submenu index="2-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="2-4-1">选项1</el-menu-item>
-          <el-menu-item index="2-4-2">选项2</el-menu-item>
-          <el-menu-item index="2-4-3">选项3</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-      <el-menu-item index="3">消息中心</el-menu-item>
-      <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
-    </el-menu>
-    <el-row class="tac">
-      <el-col :span="4">
-        <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
-          <el-submenu index="1">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>导航一</span>
-            </template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="1-1">选项1</el-menu-item>
-              <el-menu-item index="1-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="1-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="1-4">
+    <el-container>
+      <el-header>
+        <el-menu :default-active="activeIndex2" class="el-menu-demo" mode="horizontal" @select="handleSelect" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+          <el-menu-item index="1">
+            <router-link to="/" target="_blank">处理中心</router-link>
+          </el-menu-item>
+          <el-submenu index="2">
+            <template slot="title">我的工作台</template>
+            <el-submenu index="2-1">
+              <template slot="title">选项1</template>
+              <el-menu-item index="2-1-1">选项1</el-menu-item>
+              <el-menu-item index="2-1-2">选项2</el-menu-item>
+              <el-menu-item index="2-1-3">选项3</el-menu-item>
+            </el-submenu>
+            <el-menu-item index="2-2">选项2</el-menu-item>
+            <el-menu-item index="2-3">选项3</el-menu-item>
+            <el-submenu index="2-4">
               <template slot="title">选项4</template>
-              <el-menu-item index="1-4-1">选项1</el-menu-item>
+              <el-menu-item index="2-4-1">选项1</el-menu-item>
+              <el-menu-item index="2-4-2">选项2</el-menu-item>
+              <el-menu-item index="2-4-3">选项3</el-menu-item>
             </el-submenu>
           </el-submenu>
-          <el-menu-item index="2">
-            <i class="el-icon-menu"></i>
-            <span slot="title">导航二</span>
-          </el-menu-item>
-          <el-menu-item index="3" disabled>
-            <i class="el-icon-document"></i>
-            <span slot="title">导航三</span>
-          </el-menu-item>
-          <el-menu-item index="4">
-            <i class="el-icon-setting"></i>
-            <span slot="title">导航四</span>
-          </el-menu-item>
+          <el-menu-item index="3">消息中心</el-menu-item>
+          <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
         </el-menu>
-      </el-col>
-      <el-col :span="20">
-        <router-view />
-      </el-col>
-    </el-row>
+      </el-header>
+      <el-container>
+        <el-aside>
+          <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+              </template>
+              <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="1-1">选项1</el-menu-item>
+                <el-menu-item index="1-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="1-3">选项3</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="1-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="1-4-1">选项1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+            <el-menu-item index="2">
+              <i class="el-icon-menu"></i>
+              <span slot="title">导航二</span>
+            </el-menu-item>
+            <el-menu-item index="3">
+              <i class="el-icon-document"></i>
+              <span slot="title">导航三</span>
+            </el-menu-item>
+            <el-menu-item index="4">
+              <i class="el-icon-setting"></i>
+              <span slot="title">导航四</span>
+            </el-menu-item>
+          </el-menu>
+        </el-aside>
+        <el-main>
+          <router-view />
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 <script>
@@ -95,7 +92,26 @@ export default {
 
 </script>
 <style>
-  li a {
-    text-decoration: none;
-  }
+li a {
+  text-decoration: none;
+}
+
+.el-container {
+  height: 1000px;
+  margin: 0;
+}
+
+.el-header {
+  margin: 0;
+  padding: 0;
+}
+
+.el-aside {
+  background-color: #545c64;
+}
+
+.el-aside>ul {
+  width: 100%
+}
+
 </style>
--- login-demo/src/components/pages/Login.vue
+++ login-demo/src/components/pages/Login.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    <el-form ref="form" :model="form" label-width="80px">
-      <el-col :span="11">
+    <el-form ref="form" :model="form" label-width="40px" :span="8" :xs="10" :sm="10" :md="6" :lg="6" :xl="6" :offset="2">
+      <el-col>
         <el-form-item label="账号">
           <el-input v-model="form.name"></el-input>
         </el-form-item>
         <el-form-item label="密码">
           <el-input v-model="form.password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">登录</el-button>
+          <el-button>取消</el-button>
         </el-form-item>
       </el-col>
     </el-form>
@@ -24,13 +28,16 @@ export default {
     }
   },
   props: {},
-  methods: {}
+  methods: {
+    onSubmit () {
+      console.log('submit')
+    }
+  }
 }
 
 </script>
-
 <style>
-    .el-form {
-        margin-top: 5%;
-    }
+.el-form {
+  margin-top: 5%;
+}
 </style>
